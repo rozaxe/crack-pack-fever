06164c2bc69bbfd95f6005f0b4801212a5e4ad46
Axel Rb <axel@rozobrezillac.fr>
2025-11-01 21:27:27 +0100

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route, Link, } from 'react-router-dom'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 import { HomePage } from './pages/home'
 import { CrackPage } from './pages/crack'
@@ -21,6 +22,7 @@ const queryClient = new QueryClient({
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
+            <Analytics />
             <SpeedInsights />
             <BrowserRouter
                 future={{
